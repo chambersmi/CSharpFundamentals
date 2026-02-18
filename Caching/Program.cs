d6986a0d2f09f5e87924968bf5ac12859f091da6
Mike C <mjchambers83@gmail.com>
2026-02-18 18:06:01 -0500

    Caching

---
+++ Caching/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Caching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+
+    public class PeopleController
+    {
+        private readonly IRepository<Person> _peopleRepository;
+
+        public PeopleController(IRepository<Person> peopleRepository)
+        {
+            _peopleRepository = peopleRepository;
+        }
+
+        public Person? GetByName(string firstName, string lastName)
+        {
+            // This may take a long time to access this data.
+            // If we already accessed it, we can store it in application memory
+            return _peopleRepository
+                .GetByName(firstName, lastName)
+                .FirstOrDefault();
+        }
+    }
+
+    public class Person
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+
+    public interface IRepository<T>
+    {
+        List<T> GetByName(string fname, string lname);
+    }
+
+    public class Repository : IRepository<Person>
+    {
+        public List<Person> GetByName(string fname, string lname)
+        {
+            List<Person> personList = new List<Person>();
+            var p = new Person
+            {
+                FirstName = "Mike",
+                LastName = "Doe"
+            };
+
+            personList.Add(p);
+
+            return personList;
+
+        }
+    }
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Indexers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 10, 20, 30 };
+            var list = new List<decimal> { 5.5m, 3m, 0m };
+            var thirdElement = list[2];
+
+            // Instance of a custom List class
+            var numbers2 = new MyList<int>(new int[] { 1, 2, 3, 4 });
+            numbers2[2] = 0;
+
+            var string1 = numbers2["1"];
+        }
+    }
+
+    class MyList<T>
+    {
+        private T[] _elements;
+
+        public MyList(T[] elements)
+        {
+            _elements = elements;
+        }
+
+        // Define an integer indexer
+        public T this[int index]
+        {
+            get => _elements[index];
+            set => _elements[index] = value;
+        }
+
+        // Define a string indexer
+        public T this[string index]
+        {
+            get => _elements[int.Parse(index)];
+            set => _elements[int.Parse(index)] = value;
+        }
+
+        // Define indexer with multiple parameters
+        public T this[int index1, int index2]
+        {
+            get => _elements[index1 + index2];
+            set => _elements[index1 + index2] = value;
+        }
+    }
+}
